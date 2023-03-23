11d97808fe07506a941a8a3ddd0cd7ad6744494b
SvanLeeuwen5 <s.vanleeuwen5@student.avans.nl>
2023-03-23 12:39:56 +0100

    Update meldinglijst_frame.py
    werkende knopjes

--- meldinglijst_frame.py
+++ meldinglijst_frame.py
@@ -6,6 +6,7 @@ from font import FontLabel
 class MeldingLijstTableFrame(QFrame):
     def __init__(self):
         super().__init__()
+        self.itemCount = 0
         table_layout = QVBoxLayout()
         self.setLayout(table_layout)
 
@@ -14,7 +15,6 @@ class MeldingLijstTableFrame(QFrame):
                             "QHeaderView::section {color : #b2b2b2; background-color: #4c575b;}"
                             "QHeaderView::section:horizontal {color : #b2b2b2; background-color: #4c575b;}")
 
-        self.table.setRowCount(50)
         self.table.setColumnCount(7)
         self.table.setHorizontalHeaderLabels(["Type", "Cam ", "Tijdstip", "Locatie", "Melding", "Ond", "Notitie"])
         
@@ -24,16 +24,27 @@ class MeldingLijstTableFrame(QFrame):
         horizontal_header.setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
         horizontal_header.setStretchLastSection(True)
 
-        # Data aan tabel toevoegen (random bijgedaan)
-        for i in range(self.table.rowCount()):
-            for j in range(self.table.columnCount()):
-                item = QTableWidgetItem(f"Row {i}, Col {j}")
-                item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEditable)
-                self.table.setItem(i, j, item)
+        self.newItem()
+        self.newItem()
+        self.newItem()
         table_layout.addWidget(self.table)
 
-        
+    def newItem(self):
+        self.table.insertRow(0)
+        for j in range(self.table.columnCount()):
+            item = QTableWidgetItem(f"Row {self.itemCount}, Col {j}")
+            item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+            self.table.setItem(0, j, item)
+        self.itemCount += 1
 
+    def removeItem(self):
+        self.itemCount -= 1
+        self.table.removeRow(0)
+
+    def removeAllItems(self):
+        for i in range(self.itemCount):
+            self.table.removeRow(0)
+        self.itemCount = 0
 
 
 class MeldingLijstButton(QPushButton):
@@ -53,13 +64,14 @@ class MeldingLijstButtonFrame(QFrame):
         self.setLayout(button_layout)
         meldinglijst_label = FontLabel("Onbevestigde meldingen",14, True)
 
-        button_check = MeldingLijstButton("","icons/check.svg")
-        button_check_all = MeldingLijstButton("","icons/check-double.svg")
+        self.button_check = MeldingLijstButton("","icons/check.svg")
+        self.button_check_all = MeldingLijstButton("","icons/check-double.svg")
+        
 
         button_layout.addWidget(meldinglijst_label, alignment=Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignBottom)
         button_layout.addItem(self.horizontalSpacer)
-        button_layout.addWidget(button_check)
-        button_layout.addWidget(button_check_all)
+        button_layout.addWidget(self.button_check)
+        button_layout.addWidget(self.button_check_all)
 
 
 class MeldingLijst(QFrame):
@@ -74,6 +86,9 @@ class MeldingLijst(QFrame):
         
         self.tabel_lijst = MeldingLijstTableFrame()
         self.buttons = MeldingLijstButtonFrame()
+
+        self.buttons.button_check.clicked.connect(self.tabel_lijst.removeItem)
+        self.buttons.button_check_all.clicked.connect(self.tabel_lijst.removeAllItems)
 
         self.layout.addWidget(self.buttons)
         self.layout.addWidget(self.tabel_lijst)
