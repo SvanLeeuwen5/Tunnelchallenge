f7751ac52f932aef304a23f2618893f3b583a909
SvanLeeuwen5 <s.vanleeuwen5@student.avans.nl>
2023-04-05 16:19:03 +0200

    Update data parse

--- data_parse.py
+++ data_parse.py
@@ -2,105 +2,95 @@ import asyncio
 import json
 import websockets
 
-# Global State Variables
-SOS_Status = []
-CCTV_Status = []
-Lighting_Status = []
-VRI_Status = []
-Barrier_Status = []
-MSI_Status = []
-
-# Send Request Command defitions in the form: Control_lfv
-
-# parameter definitions
-def Control_SOS():
-    return 0
-
-
-# parameter definitions
-def Control_CCTV():
-    return 0
-
-
-# parameter definitions
-def Control_Lighting():
-    return 0
+class DataParser:
     
+    def __init__(self):
+        self.sos_state = []
+        self.cctv_state = []
+        self.lighting_state = []
+        self.vri_state = []
+        self.barrier_state = []
+        self.msi_state = []
+        asyncio.run(self.UpdateAllStatusses())
 
-# command: on off   id: 0 1 2 3
-def Control_VRI(command, id) :
-    return {"action": "command", "lfv": "vri", "command" : command, "id": id}
+    async def UpdateAllStatusses(self):
+        async with websockets.connect("ws://213.93.142.164:8080") as websocket:
+            await self.GetStatus_SOS(websocket)
+            await self.GetStatus_CCTV(websocket)
+            await self.GetStatus_Lighting(websocket)
+            await self.GetStatus_VRI(websocket)
+            await self.GetStatus_Barrier(websocket)
+            await self.GetStatus_MSI(websocket)
 
+    def Control_SOS(self):
+        return 0
 
-# parameter definitions
-def Control_Barrier():
-    return 0
+    # parameter definitions
+    def Control_CCTV(self):
+        return 0
 
-# parameter definitions
-def Control_MSI():
-    return 0
+    # parameter definitions
+    def Control_Lighting(self):
+        return 0
 
-
-# Retrieving Status commando in the form: lfv
-async def GetStatus_SOS(ws):
-    command = {"action": "status", "lfv": "sos"}
-    json_string = await send_command(ws, command)
-    json_obj = json.loads(json_string)
-    global SOS_Status
-    #TODO
-
-
-async def GetStatus_CCTV(ws):
-    command = {"action": "status", "lfv": "cctv"}
-    json_string = await send_command(ws, command)
-    json_obj = json.loads(json_string)
-    global CCTV_Status
-    #TODO
-
-async def GetStatus_Lighting(ws):
-    command = {"action": "status", "lfv": "lighting"}
-    json_string = await send_command(ws, command)
-    json_obj = json.loads(json_string)
-    global Lighting_Status
-    #TODO
+    # command: on off   id: 0 1 2 3
+    def Control_VRI(self,command, id) :
+        return {"action": "command", "lfv": "vri", "command" : command, "id": id}
 
 
-async def GetStatus_VRI(ws):
-    command = {"action": "status", "lfv": "vri"}
-    json_string = await send_command(ws, command)
-    json_obj = json.loads(json_string)
-    global VRI_Status 
-    VRI_Status = [d["state"] for d in json_obj["data"]]
+    # parameter definitions
+    def Control_Barrier(self):
+        return 0
 
-async def GetStatus_Barrier(ws):
-    command = {"action": "status", "lfv": "barrier"}
-    json_string = await send_command(ws, command)
-    json_obj = json.loads(json_string)
-    global Barrier_Status
-    #TODO
+    # parameter definitions
+    def Control_MSI(self):
+        return 0
 
-async def GetStatus_MSI(ws):
-    command = {"action": "status", "lfv": "msi"}
-    json_string = await send_command(ws, command)
-    json_obj = json.loads(json_string)
-    global MSI_Status
-    #TODO
+    # Retrieving Status commando in the form: lfv
+    async def GetStatus_SOS(self,ws):
+        command = {"action": "status", "lfv": "sos"}
+        json_string = await self.send_command(ws, command)
+        json_obj = json.loads(json_string)
+        self.sos_state #= [d["state"] for d in json_obj["data"]]
 
 
-async def send_command(websocket, command):
-    await websocket.send(json.dumps(command))
-    return await websocket.recv()
+    async def GetStatus_CCTV(self,ws):
+        command = {"action": "status", "lfv": "cctv"}
+        json_string = await self.send_command(ws, command)
+        json_obj = json.loads(json_string)
+        self.cctv_state #= [d["state"] for d in json_obj["data"]]
+
+    async def GetStatus_Lighting(self,ws):
+        command = {"action": "status", "lfv": "lighting"}
+        json_string = await self.send_command(ws, command)
+        json_obj = json.loads(json_string)
+        self.lighting_state #= [d["state"] for d in json_obj["data"]]
 
 
-async def UpdateAllStatusses():
-    async with websockets.connect("ws://213.93.142.164:8080") as websocket:
-        await GetStatus_SOS(websocket)
-        await GetStatus_CCTV(websocket)
-        await GetStatus_Lighting(websocket)
-        await GetStatus_VRI(websocket)
-        await GetStatus_Barrier(websocket)
-        await GetStatus_MSI(websocket)
+    async def GetStatus_VRI(self,ws):
+        command = {"action": "status", "lfv": "vri"}
+        json_string = await self.send_command(ws, command)
+        json_obj = json.loads(json_string)
+        print(json_obj)
+        self.vri_state = [d["state"] for d in json_obj["data"]]
+
+
+    async def GetStatus_Barrier(self, ws):
+        command = {"action": "status", "lfv": "barrier"}
+        json_string = await self.send_command(ws, command)
+        json_obj = json.loads(json_string)
+        self.barrier_state #= [d["state"] for d in json_obj["data"]]
+
+    async def GetStatus_MSI(self, ws):
+        command = {"action": "status", "lfv": "msi"}
+        json_string = await self.send_command(ws, command)
+        json_obj = json.loads(json_string)
+        self.msi_state #= [d["state"] for d in json_obj["data"]]
+
+
+    async def send_command(self, websocket, command):
+        await websocket.send(json.dumps(command))
+        return await websocket.recv()
         
 
-if __name__ == "__main__":
-    asyncio.run(UpdateAllStatusses())
+
